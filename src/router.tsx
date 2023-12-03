4f5518904525440bd68246d8fa9c3543666bfc06
BigT <73481585+0BigT@users.noreply.github.com>
2023-12-03 15:56:10 +0100

    Routing handlers
    + Routing error handler
    + Not found page
    + Logo
    + Favicon

--- src/router.tsx
+++ src/router.tsx
@@ -1,16 +1,18 @@
 import {createBrowserRouter, createRoutesFromElements, Outlet, Route} from 'react-router-dom';
-// import NotFound from 'UI/App/Partials/App/NotFound';
-// import RoutingErrorHandler from 'UI/App/Partials/App/RoutingErrorHandler';
+import NotFound from 'partials/App/NotFound';
+import RoutingErrorHandler from 'partials/App/RoutingErrorHandler';
 import './assets/styling/global.css';
 import Home from 'pages/Home';
 
-
 export default createBrowserRouter(
     createRoutesFromElements(
-        <Route path="/" errorElement={"Error"}>
+        <Route path="/" errorElement={<RoutingErrorHandler />}>
 
             {/* public routes */}
             <Route index element={<Home />} />
+
+            {/* 404 */}
+            <Route path="*" element={<NotFound />} />
 
         </Route>
     )
